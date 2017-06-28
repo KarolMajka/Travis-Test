a4d7b59df94e99afc19cb8fc317051b576a1f60a
Karol <karolmajka95@gmail.com>
2017-06-28 09:22:52 +0200

    test fail

--- Travis-TestTests/Travis_TestTests.swift
+++ Travis-TestTests/Travis_TestTests.swift
@@ -23,6 +23,7 @@ class Travis_TestTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(1, 2, "asser fail")
     }
     
     func testPerformanceExample() {
